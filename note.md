0b2e38f7c7e7bfd4194790183c9e5885d659b616
royes <yanyx99328@163.com>
2020-06-27 17:28:21 +0800

    Modify serial version: add time measure, add repeated running of a case

--- note.md
+++ note.md
@@ -7,7 +7,10 @@ To-do list:
 - [x] Complete serial code. Verify it.
 - [ ] Complete CUDA code.  
 
----
+## Details
+
+- Hotspot 是读写.
+- 为了可以对同一个 case 进行多次测试, 把读取的数组拷贝到另一个数组, 再进行计算. 这会引入内存读写的额外时间, 但是运行次数较多的情况下, 额外运行时间不大, 可以不太考虑.
 
 ## Machines
 
--- src/serial.cc
+++ src/serial.cc
@@ -1,4 +1,8 @@
 #include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include<cstring>
 
 void HinesAlgo (
     double *u, double *l, double *d,
@@ -23,25 +27,26 @@ void HinesAlgo (
 
 int main(int argc, char * argv[]) {
     FILE *fp;
+    clock_t time;
+    int repeatNum;
 
-    int *id;
-    double *u;
-    double *l;
-    double *d;
-    double *rhs;
-    int *p;
+    int *id; double *u; double *l;
+    double *d; double *rhs; int *p;
+
+    int *id1; double *u1; double *l1;
+    double *d1; double *rhs1; int *p1;
+
     int N;
     
     fp = fopen(argv[1], "r");
 
     fscanf(fp, "%d", &N);
 
-    id = new int [N];
-    u = new double [N];
-    l = new double [N];
-    d = new double [N];
-    rhs = new double [N];
-    p = new int [N];
+    id = new int [N]; u = new double [N]; l = new double [N];
+    d = new double [N]; rhs = new double [N]; p = new int [N];
+
+    id1 = new int [N]; u1 = new double [N]; l1 = new double [N];
+    d1 = new double [N]; rhs1 = new double [N]; p1 = new int [N];
 
     for (int i=0;i<N;++i) {
         fscanf(
@@ -51,22 +56,41 @@ int main(int argc, char * argv[]) {
 
     fclose(fp);
 
-    HinesAlgo(u ,l, d, rhs, p, N);
+    if (argc==4) {
+        repeatNum = atoi(argv[3]);
+    } else {
+        repeatNum = 1;
+    }
+
+    time = clock();
+
+    for (int i=0;i<repeatNum;++i) {
+        memcpy(u1, u, N*sizeof(double));
+        memcpy(l1, l, N*sizeof(double));
+        memcpy(d1, d, N*sizeof(double));
+        memcpy(rhs1, rhs, N*sizeof(double));
+        memcpy(p1, p, N*sizeof(int));
+
+        HinesAlgo(u1 ,l1, d1, rhs1, p1, N);
+    }
+
+    time = clock() - time;
+
+    printf("Serial time cost: %.2f secomds.\n", static_cast<double>(time)/CLOCKS_PER_SEC);
 
     fp = fopen(argv[2], "w+");
 
     for (int i=0;i<N;++i) {
         fprintf(
             fp, "%d %lf %lf %lf %lf\n",
-            id[i], u[i], l[i], rhs[i], d[i]);
+            id1[i], u1[i], l1[i], rhs1[i], d1[i]);
     }
 
     fclose(fp);
 
-    delete[] id;
-    delete[] u;
-    delete[] l;
-    delete[] d;
-    delete[] rhs;
-    delete[] p;
+    delete[] id; delete[] u; delete[] l;
+    delete[] d; delete[] rhs; delete[] p;
+
+    delete[] id1; delete[] u1; delete[] l1;
+    delete[] d1; delete[] rhs1; delete[] p1;
 }
